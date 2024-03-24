dcb97e19a7725616b4899609b18dd3f80b984f4d
luciano <rodriguezluciano761@gmail.com>
2024-03-24 18:29:07 -0300

    connection to  mongodb atlas in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import handlebars from "express-handlebars";
-import __dirname from "./utils.js";
 import { Server } from "socket.io";
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+import __dirname from "./utils.js";
 import productRouter from "./routes/products.routes.js";
 import cartRouter from "./routes/cart.routes.js";
 import viewsRouter from "./routes/views.routes.js";
@@ -63,3 +66,15 @@ app.engine("handlebars", handlebars.engine());
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "handlebars");
 app.use("/", viewsRouter);
+
+//MONGO DB CONEXION y DO
+dotenv.config();
+
+mongoose.connect(process.env.URL_MDBATLAS)
+.then(() => {
+  console.log('Conexión exitosa a la base de datos');
+})
+.catch(error => {
+  console.log("No se puede conectar a la base de datos: " + error);
+  process.exit(1); // Salir del proceso con un código de error
+});
