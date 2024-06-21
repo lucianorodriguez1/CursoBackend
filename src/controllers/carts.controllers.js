064891319307a480e9e936fc10cabe6098de6f92
luciano <rodriguezluciano761@gmail.com>
2024-06-21 16:03:16 -0300

    crear metodos service sessions y mejorar controller services

--- src/controllers/carts.controllers.js
+++ src/controllers/carts.controllers.js
@@ -2,50 +2,50 @@ import cartsService from "../services/cartsServices.js";
 import { response } from "../utils/response.js";
 
 export const getCarts = async (req, res) => {
-    const data = await cartsService.getCarts();
-    response(res,200,data);
+  const data = await cartsService.getCarts();
+  response(res, 200, data);
 };
 export const createCart = async (req, res) => {
-    const data = await cartsService.createCart();
-    response(res,201,data);
+  const data = await cartsService.createCart();
+  response(res, 201, data);
 };
 export const getCartById = async (req, res) => {
-    const id = req.params.cid;
-    const data = await cartsService.getCartById(id);
-    response(res,200,data);
+  const id = req.params.cid;
+  const data = await cartsService.getCartById(id);
+  response(res, 200, data);
 };
 export const addProductFromCart = async (req, res) => {
-    const idCart = req.params.cid;
-    const idProduct = req.params.pid;
-    const data = await cartsService.addProductFromCart(idCart, idProduct);
-    response(res,200,data);
+  const idCart = req.params.cid;
+  const idProduct = req.params.pid;
+  const data = await cartsService.addProductFromCart(idCart, idProduct);
+  response(res, 200, data);
 };
 export const deleteAllProductsFromCartById = async (req, res) => {
-    const { cid } = req.params;
-    const data = await cartsService.deleteProductsCart(cid);
-   response(res,200,data)
+  const { cid } = req.params;
+  const data = await cartsService.deleteProductsCart(cid);
+  response(res, 200, data)
 };
 export const deleteProductFromCart = async (req, res) => {
-    const { cid, pid } = req.params;
-    const data = cartsService.deleteProduct(cid, pid);
-    response(res,200,data);
+  const { cid, pid } = req.params;
+  const data = cartsService.deleteProduct(cid, pid);
+  response(res, 200, data);
 };
 export const updateCartById = async (req, res) => {
-    const { products } = req.body;
-    const { cid } = req.params;
-    const data = await cartsService.updateProductsCart(cid, products);
-    response(res,200,data)
+  const { products } = req.body;
+  const { cid } = req.params;
+  const data = await cartsService.updateProductsCart(cid, products);
+  response(res, 200, data)
 };
 export const updateProductCart = async (req, res) => {
-    const { cid, pid } = req.params;
-    const { quantity } = req.body;
-    const data = await cartsService.updateProduct(cid, pid, quantity);
-    response(res,200,data)
+  const { cid, pid } = req.params;
+  const { quantity } = req.body;
+  const data = await cartsService.updateProduct(cid, pid, quantity);
+  response(res, 200, data)
 };
 
-export const purchase = async(req,res) =>{
-    const { cid} = req.params;
-    const data = await cartsService.purchaseCart(cid);
-    response(res,200,data)
+export const purchase = async (req, res) => {
+  const { cid } = req.params;
+  const data = await cartsService.purchaseCart(cid);
+  response(res, 200, data)
 }
 
---
+++ src/services/sessionsServices.js
@@ -0,0 +1,57 @@
+import { cartsRepository } from "../repositories/index.js";
+import { usersRepository } from "../repositories/index.js";
+import { generateToken } from "../utils/jwt.js";
+import { isValidPassword, createHash } from "../utils/bcrypt.js";
+
+class SessionService {
+    constructor() { }
+
+    async login(email, password) {
+        const user = await usersRepository.getUserByEmail(email);
+
+        if (!user)
+            return res.render("login", { error: "credenciales incorrectas" }); //probar error
+
+        const validatePassword = isValidPassword(user, password);
+        if (!validatePassword)
+            return res.render("login", { error: "credenciales incorrectas" }); //probar error
+
+        const token = generateToken(user);
+        res
+            .cookie("coderCookieToken", token, {
+                maxAgre: 60 * 60 * 1000,
+                httpOnly: true,
+            })
+    }
+    async register(data) {
+        //probar si email existe
+        const passwordHash = createHash(data.password);
+        const cartObject = await cartsRepository.createCart();
+        const cartId = cartObject[0]._id;
+        const newUser = await usersRepository.createUser({
+            first_name,
+            last_name,
+            age,
+            email,
+            password: passwordHash,
+            cartId: cartId,
+        });
+        const user = await usersRepository.getUserByEmail(email);
+        const token = generateToken(user);
+        res
+            .cookie("coderCookieToken", token, {
+                maxAgre: 60 * 60 * 1000,
+                httpOnly: true,
+            })
+    }
+    async logout(req, res) {
+        res.clearCookie("coderCookieToken");
+    }
+
+    async current(req) {
+        return req.user
+    }
+}
+
+const sessionService = new SessionService();
+export default sessionService;
