7ed11e77c31b2988cf1ec1203193afc3bde6d425
luciano <lucianorodriguez1@users.noreply.github.com>
2024-06-21 00:50:47 -0300

    controller y service user terminado (1era version)

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,51 +1,30 @@
-import { usersRepository } from "../repositories/index.js";
-import {createHash} from "../utils/bcrypt.js"
+import usersServices from "../services/usersServices.js";
+import { createHash } from "../utils/bcrypt.js";
 import { response } from "../utils/response.js";
 
 export const getUsers = async (req, res) => {
-  try {
-    const users = await usersRepository.getUsers();
-    res.json(users);
-  } catch (error) {
-    console.log(error);
-  } 
+  const users = await usersServices.getUsers();
+  response(res, 200, users);
 };
-export const getUser = async (req, res) => {  
-  try {
-    const id = req.params.uid;
-    const user = await usersRepository.getUserById(id);
-    res.status(200).json({status:"succes",payload:user});
-  } catch (error) {
-    console.log(error);
-  }
-};
-export const createUser = async (req, res) => {
-  try {
-    const { first_name, last_name, age, email, password } = req.body;
-    let newUser;
-    let passwordHash = createHash(password);
 
-    if (email == "adminCoder@coder.com" && password == "adminCod3r123") {
-      newUser = await usersRepository.createUser({
-        first_name,
-        last_name,
-        age,
-        email,
-        password:createHash(password),
-        rol: "admin",
-      });
-    } else {
-      newUser = await usersRepository.createUser({
-        first_name,
-        last_name,
-        age,
-        email,
-        password:passwordHash
-      });
-    }
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.log(error);
-  }
+export const getUser = async (req, res) => {
+  const id = req.params.uid;
+  const user = await usersServices.getUserById(id);
+  response(res, 200, user);
 };
- 
+
+export const createUser = async (req, res) => {
+  const { first_name, last_name, age, email, password } = req.body;
+
+  const passwordHash = createHash(password);
+
+  const newUser = await usersServices.createUser({
+    first_name,
+    last_name,
+    age,
+    email,
+    password: passwordHash,
+  });
+
+  response(res,201,newUser);
+};
--- src/services/usersServices.js
+++ src/services/usersServices.js
@@ -1,126 +1,27 @@
-import { productsRepository } from "../repositories/index.js";
-import productModel from "../dao/mongo/models/product.model.js";
-import CustomError from "./errors/CustomError.js";
-import { ErrorCodes } from "./errors/enums.js";
-import { generateProductErrorInfo } from "./errors/info.js";
+import { usersRepository } from "../repositories/index.js";
 
 class UserService {
   constructor() {}
 
-  async getProducts(limit, page, sort, query) {
-    let result = await productsRepository.getProducts(limit, page, sort, query);
+  async getUsers() {
+    let result = await usersRepository.getUsers();
     return result;
   }
 
-  async codeExists(product, excludeId = null) {
-    const query = { code: product.code };
-    if (excludeId) {
-      query._id = { $ne: excludeId };
-    }
-    return await productModel.findOne(query);
-  }
-
-  async createProduct(product) {
-    if (
-      !product.title ||
-      !product.description ||
-      !product.code ||
-      !product.status ||
-      !product.stock ||
-      !product.category ||
-      !product.thumbnail
-    ) {
-      CustomError.createError({
-        name: "Product creation error",
-        cause: generateProductErrorInfo({
-          title,
-          description,
-          code,
-          price,
-          status,
-          stock,
-          category,
-          thumbnail,
-        }),
-        message: "Error trying to create Product",
-        code: ErrorCodes.INVALID_TYPES_ERROR,
-      });
-    }
-    const codeExists = await this.codeExists(product);
-    if (codeExists) {
-      CustomError.createError({
-        name: "Product with this code already exists",
-        cause: "code duplicado",
-        message: "Error trying to create Product",
-        code: ErrorCodes.DUPLICATE_CODE,
-      });
-    }
-    let result = await productsRepository.createProduct(product);
+  async createUser(user) {
+    let result = await usersRepository.createUser(user);
     return result;
   }
 
-  async getProductById(id) {
-    let result = await productsRepository.getProductById(id);
-    if (!result)
-      CustomError.createError({
-        name: "INVALID ID",
-        cause: "invalid id",
-        message: "Error get product",
-        code: ErrorCodes.INVALID_ID,
-      });
+  async getUserById(id) {
+    let result = await usersRepository.getUserById(id);
     return result;
   }
-
-  async deleteProductById(id) {
-    let result = await productsRepository.deleteProductById(id);
-    if (!result)
-      CustomError.createError({
-        name: "INVALID ID",
-        cause: "invalid id",
-        message: "Error get product",
-        code: ErrorCodes.INVALID_ID,
-      });
+  async getUserByEmail(email) {
+    let result = await usersRepository.getUserByEmail(email);
     return result;
-  }
-
-  //1.que no permita campos vacios
-  async updateProductById(id, data) {
-    const codeExists = await this.codeExists(data, id);
-    if (codeExists) {
-      CustomError.createError({
-        name: "Product with this code already exists",
-        cause: "code duplicado",
-        message: "Error trying to update Product",
-        code: ErrorCodes.DUPLICATE_CODE,
-      });
-    }
-
-    this.removeEmptyFields(data);
-    let result = await productsRepository.updateProductById(id, data);
-    if (!result)
-      CustomError.createError({
-        name: "INVALID ID",
-        cause: "invalid id",
-        message: "Error get product",
-        code: ErrorCodes.INVALID_ID,
-      });
-    return result;
-  }
-  removeEmptyFields(obj) {
-    for (let key in obj) {
-      if (
-        obj[key] === null || 
-        obj[key] === undefined || 
-        obj[key] === '' || 
-        (Array.isArray(obj[key]) && obj[key].length === 0) || 
-        (typeof obj[key] === 'object' && Object.keys(obj[key]).length === 0)
-      ) {
-        delete obj[key];
-      }
-    }
-    return obj;
   }
 }
 
-const productsService = new ProductService();
-export default productsService;
+const usersServices = new UserService();
+export default usersServices;
