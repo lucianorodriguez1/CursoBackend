63beb7c831afc5bdaba15c183af10c9e1cd7fa7d
luciano <lucianorodriguez1@users.noreply.github.com>
2024-06-24 15:09:46 -0300

    agregar campo documents con name y reference al modelo user

--- src/dao/mongo/models/user.model.js
+++ src/dao/mongo/models/user.model.js
@@ -1,51 +1,61 @@
-import mongoose from 'mongoose';
- 
-const userCollection = 'users';
+import mongoose from "mongoose";
+
+const userCollection = "users";
 
 const userSchema = new mongoose.Schema({
   first_name: {
     type: String,
-    minLength:3,
+    minLength: 3,
     required: true,
   },
   last_name: {
     type: String,
-    minLength:3,
+    minLength: 3,
     required: true,
   },
   age: {
     type: Number,
-    min:18,
+    min: 18,
     required: true,
   },
   email: {
     type: String,
-    minLength:3,
+    minLength: 3,
     required: true,
     unique: true,
   },
   password: {
     type: String,
-    minLength:4,
+    minLength: 4,
     required: true,
   },
-  cartId:{
+  cartId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'carts',
+    ref: "carts",
     required: true,
   },
   role: {
-    type: String, 
-    enum: ['user', 'admin', 'premium'],
-    default: 'user',
+    type: String,
+    enum: ["user", "admin", "premium"],
+    default: "user",
     required: true,
   },
+  documents:{ type:[
+    {
+      name: {
+        type: String,
+      },
+      reference: {
+        type: String,
+      },
+    },
+  ],default:[]},
 });
 
-userSchema.pre('find', function () {
-  this.populate('cartId');
+userSchema.pre("find", function () {
+  this.populate("cartId");
 });
 
-const userModel = mongoose.model(userCollection,userSchema);
+const userModel = mongoose.model(userCollection, userSchema);
 
 export default userModel;
