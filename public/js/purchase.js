206f4100ebd5ac5f08af7471d15913e91d310811
luciano <lucianorodriguez1@users.noreply.github.com>
2024-07-12 21:06:37 -0300

    realizar la compra desde el front. crear archivo purchase.js

---
+++ public/js/purchase.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const purchaseBtn = document.getElementById("purchaseBtn");
+  if (purchaseBtn) {
+    purchaseBtn.addEventListener("click", async (e) => {
+      e.preventDefault();
+
+      try {
+
+        const response = await fetch("/api/cart", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Error al obtener el carrito");
+        }
+        const data = await response.json();
+        const url = `http://localhost:8080/api/purchases/${data.cartId}`;
+        const responsePurch = await fetch(
+          url,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({}),
+          }
+        );
+
+        const dataPurch = await responsePurch.json();
+
+        if (responsePurch.ok) {
+          alert("Se realizo la compra");
+          location.reload();
+        } else {
+          throw new Error(dataPurch.message || "error en la compra");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        alert(
+          "Hubo un problema al hacer la compra. Por favor, inténtalo de nuevo más tarde."
+        );
+      }
+    });
+  }
+});
