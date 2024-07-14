9f47a867cb2f9054e149f0604cd2ba5c6d8245ff
luciano <rodriguezluciano761@gmail.com>
2024-07-13 22:40:56 -0300

    realizar testing de crud de productos con supertest

--- test/supertest.test.js
+++ test/supertest.test.js
@@ -10,8 +10,9 @@ const requester = supertest(`${config.AppUrl}:${config.port}`);
 mongoose.connect(config.mongoUrl);
 
 describe("Testing E-commerce", async function () {
-    let cookie;
-    before(async function () {    
+  let cookie;
+  let productId;
+  before(async function () {
     const loginResponse = await requester.post("/api/sessions/login").send({
       email: "admin@gmail.com",
       password: "admin",
@@ -20,33 +21,71 @@ describe("Testing E-commerce", async function () {
     if (cookies) {
       cookie = cookies[0].split(';')[0];
     }
-});
 
-/*
-beforeEach(function () {
+  });
+
+  describe("Test de productos", function () {
     this.timeout(5000);
-});
-*/
-describe("Test de productos", function () {
-    this.timeout(5000);
+
     it("POST /api/products debe crear un producto correctamente", async function () {
       const productMock = {
-        title: "producto test 1",
-        description: "descripcion del producto test 1",
-        code: 9,
-        price: 250,
+        title: "producto test 2",
+        description: "descripcion del producto test 2",
+        code: 10,
+        price: 300,
         status: true,
-        stock: 1,
+        stock: 5,
         category: "test",
         thumbnail: [],
       };
       const { statusCode, ok, body } = await requester
         .post("/api/products")
-        .set("Authorization", `Bearer ${cookie}`)
+        .set("Cookie", cookie)
         .send(productMock);
       expect(statusCode).to.equal(201);
       expect(ok).to.be.true;
-      expect(body).to.have.property("title", "producto test 1");
+      expect(body.data).to.have.property("title", "producto test 2");
+      productId = body.data.id
+    });
+
+    it("GET /api/products debe obtener todos los productos correctamente", async function () {
+      const { statusCode, ok, body } = await requester
+        .get("/api/products")
+        .set("Cookie", cookie)
+      expect(statusCode).to.equal(200);
+      expect(ok).to.be.true;
+    });
+
+    it("GET /api/products/:pid debe obtener un producto por su ID", async function () {
+      const { statusCode, ok, body } = await requester
+        .get(`/api/products/${productId}`)
+        .set("Cookie", cookie)
+      expect(statusCode).to.equal(200);
+      expect(ok).to.be.true;
+      expect(body.data).to.have.property("id", productId);
+    });
+
+    it("PUT /api/products/:pid debe actualizar un producto por su ID", async function () {
+      const updateMock = {
+        title: "producto actualizado",
+        description: "descripcion actualizada",
+        price: 400,
+        stock: 10,
+      };
+      const { statusCode, ok, body } = await requester
+        .put(`/api/products/${productId}`)
+        .set("Cookie", cookie)
+        .send(updateMock);
+      expect(statusCode).to.equal(200);
+      expect(ok).to.be.true;
+    });
+
+    it("DELETE /api/products/:pid debe eliminar un producto por su ID", async function () {
+      const { statusCode, ok, body } = await requester
+        .delete(`/api/products/${productId}`)
+        .set("Cookie", cookie)
+      expect(statusCode).to.equal(200);
+      expect(ok).to.be.true;
     });
   });
 });
