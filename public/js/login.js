47dee42a0a8db6aa995cef7c3268188ecdfd86c5
luciano <lucianorodriguez1@users.noreply.github.com>
2024-07-19 14:33:58 -0300

    comentar error de login para probar

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.getElementById("loginForm");
-  const errorMessage = document.getElementById("error-message");
+  //const errorMessage = document.getElementById("error-message");
 
   loginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -8,14 +8,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const email = loginForm.email.value;
     const password = loginForm.password.value;
     try {
-      const response = await fetch("https://cursobackend-production-680d.up.railway.app/api/sessions/login", {
+      const response = await fetch("/api/sessions/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ email, password }),
       });
-      
       const data = await response.json();
 
       if (response.ok) {
