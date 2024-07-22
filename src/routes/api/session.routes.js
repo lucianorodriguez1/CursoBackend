3122fca1497b2f7ddb82cd29c3c8d37d1904e4b3
luciano <lucianorodriguez1@users.noreply.github.com>
2024-07-22 20:47:21 -0300

    agregar validador de register. Mostrar los mensajes de error en el front.

--- src/routes/api/session.routes.js
+++ src/routes/api/session.routes.js
@@ -4,10 +4,11 @@ import { passportCall } from "../../middlewares/passportMiddleware.js";
 import { sessions } from "../../controllers/index.js";
 import { authorization } from "../../middlewares/authMiddleware.js";
 import { validateCreateLogin } from "../../utils/validator/login.js";
+import { validateCreateRegister } from "../../utils/validator/register.js";
 
 const sessionRouter = Router();
 
-sessionRouter.post("/register", sessions.register);
+sessionRouter.post("/register", validateCreateRegister,sessions.register);
 sessionRouter.post("/login", validateCreateLogin,sessions.login);
 sessionRouter.get("/logout", passportCall("jwt"),sessions.logout);
 sessionRouter.get("/current", passportCall("jwt"), sessions.current);
