38b10d55fd45268d40b1e99fa0923aefdf047acb
luciano <lucianorodriguez1@users.noreply.github.com>
2024-07-23 12:29:20 -0300

    agregar permisos opcional en el getProductById

--- src/routes/api/products.routes.js
+++ src/routes/api/products.routes.js
@@ -9,7 +9,7 @@ const productRouter = Router();
 
 productRouter.get('/', passportCallOptional('jwt'),products.getProducts);
 productRouter.post('/', passportCall('jwt'),authorization('admin','premium'), uploader.array('thumbnails', 5),validateCreateProduct,products.createProduct);
-productRouter.get('/:pid', products.getProductById);
+productRouter.get('/:pid', passportCallOptional('jwt'),products.getProductById);
 productRouter.put('/:pid',  passportCall('jwt'),authorization('admin','premium'),products.updateProductById);
 productRouter.delete('/:pid',  passportCall('jwt'),authorization('admin','premium'), products.deleteProductById);
 productRouter.post('/:pid/images', uploader.array('products', 5), products.uploadProductImages);
