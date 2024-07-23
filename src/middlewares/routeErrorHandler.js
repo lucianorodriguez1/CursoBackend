9c71afe0dd111b9dd40a5dbed67d77519a0e0885
luciano <lucianorodriguez1@users.noreply.github.com>
2024-07-23 10:14:57 -0300

    mostrar vista no disponible en el front y back cuando se ingresa una ruta no existente

--- src/middlewares/routeErrorHandler.js
+++ src/middlewares/routeErrorHandler.js
@@ -2,10 +2,5 @@ import { ErrorCodes } from "../utils/errors/enums.js";
 import customError from "../utils/errors/CustomError.js";
 
 export default (req, res, next) => {
-  customError.createError({
-    name: "Route not found",
-    cause:"the route was entered incorrectly ruta",
-    message: "route not exists",
-    code: ErrorCodes.ROUTING_ERROR
-  });
+  res.render("not-available",{})
 };
